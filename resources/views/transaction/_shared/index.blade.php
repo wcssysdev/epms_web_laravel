cb021b4694f966d62ea9b4fcc4738cff0f260d58
EPMS Dev <dev@epms.local>
2026-09-18 10:15:16 +0700

    fix: apply robust delete handler to transaction shared datatables

--- resources/views/transaction/_shared/index.blade.php
+++ resources/views/transaction/_shared/index.blade.php
@@ -165,7 +165,7 @@ function txTable() {
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
                             </svg>
                         </a>
-                        <button onclick="this.closest('[x-data]').__x.$data.confirmDelete('${editBase}/${r.id}')"
+                        <button onclick="window.confirmTxDelete('${editBase}/${r.id}')"
                                 class="btn-action btn-danger" title="Delete">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
