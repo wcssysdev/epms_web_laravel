bbbf963d558942cd93c6c54855392ccadaef17d3
EPMS Dev <dev@epms.local>
2026-09-18 10:12:24 +0700

    fix: robust delete confirmation handler in grouping datatable avoiding __x access error

--- resources/views/admin/grouping/_shared/index.blade.php
+++ resources/views/admin/grouping/_shared/index.blade.php
@@ -304,6 +304,9 @@ function groupingTable() {
         showDelete: false,
         deleteUrl:  '',
         init() {
+            window.confirmGroupingDelete = (url) => {
+                this.confirmDelete(url);
+            };
             const cols = @json(array_keys($columns));
             const editBase  = '{{ url(str_replace(".", "/", $routePrefix)) }}';
             const hasQr     = {{ ($hasQr ?? false) ? 'true' : 'false' }};
@@ -330,7 +333,7 @@ function groupingTable() {
                     <div class="flex gap-1">
                         ${editBtn}
                         ${qrBtn}
-                        <button onclick="this.closest('[x-data]').__x.$data.confirmDelete('${editBase}/${r.id}')"
+                        <button onclick="window.confirmGroupingDelete('${editBase}/${r.id}')"
                                 class="btn-action btn-danger" title="Delete">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
